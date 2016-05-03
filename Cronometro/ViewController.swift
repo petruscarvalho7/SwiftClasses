8539c6cfbc7e9759f5d4354beccdea1628a95911
Petrus Carvalho <petrus.carvalho7@gmail.com>
2016-05-03 16:13:18 -0300

    Initial Commit

---
+++ Cronometro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cronometro
+//
+//  Created by Petrus Carvalho on 03/05/16.
+//  Copyright © 2016 Petrus Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
